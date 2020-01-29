411b955119e0359056532df659c2cf368fd978f4
Valentina-Aktanko <freyalhv@gmail.com>
2020-01-29 16:21:22 +0300

    функциональность формы вернулась в объект Form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,25 +7,31 @@ import { Button } from 'components/Button';
 export class App extends Component {
   state = {
     address: "http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}",
+    addressMax: "http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}",
+    
     error: null,
     isLoaded: false,
+    
     showForm: false,
+    
     userDataArray: [],
-    newUser: {
-      id: '',
-      firstName: '',
-      lastName: '',
-      email: '',
-      phone: '',
-    },
-    validation: {
-      id: false,
-      firstName: false,
-      lastName: false,
-      email: false,
-      phone: false,
-      form: false,
-    },
+    
+    // newUser: {
+    //   id: '',
+    //   firstName: '',
+    //   lastName: '',
+    //   email: '',
+    //   phone: '',
+    // },
+
+    // validation: {
+    //   id: false,
+    //   firstName: false,
+    //   lastName: false,
+    //   email: false,
+    //   phone: false,
+    //   form: false,
+    // },
   }
 
   componentDidMount() {
@@ -55,82 +61,19 @@ export class App extends Component {
     });
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
+  handleSubmit = (newData) => {
+    // const { newData } = this.props;
+
     this.setState(prevState => {
       let userDataArray = prevState.userDataArray;
-      let newUser = Object.assign({}, prevState.newUser);
-      userDataArray.unshift(newUser);
+      
+      userDataArray.unshift(newData);
       return { userDataArray };
     });
   }
 
-  handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-
-    this.setState(prevState => 
-      {
-        let newUser = Object.assign({}, prevState.newUser);
-        newUser[name] = value;
-        return { newUser };
-      },
-      () => {this.validateField(name, value)}
-    );
-  }
-
-  validateField = (fieldName, value) => {
-    let isValid = false;
-
-    // Валидация просто на заполненность
-    switch(fieldName) {
-      case 'id':
-        // TODO валидация поля id
-        isValid = Boolean(value);
-        break;
-      case 'firstName':
-        // TODO валидация поля firstName
-        isValid = Boolean(value);
-        break;
-      case 'lastName':
-        // TODO валидация поля lastName
-        isValid = Boolean(value);
-        break;
-      case 'email':
-        // TODO валидация поля email
-        isValid = Boolean(value);
-        break;
-      case 'phone':
-        // TODO валидация поля phone
-        isValid = Boolean(value);
-        break;
-    }
-        
-    this.setState(prevState => {
-      let validation = Object.assign({}, prevState.validation);
-      validation[fieldName] = isValid;
-      return { validation };
-    }, this.validateForm);
-  }
-
-  validateForm = () => {
-    this.setState(prevState => {
-      let validation = Object.assign({}, prevState.validation);
-      let isValid = (prevState.validation.id && 
-        prevState.validation.firstName && 
-        prevState.validation.lastName && 
-        prevState.validation.email && 
-        prevState.validation.phone);
-      validation.form = isValid;
-      return { validation };
-    });
-  }
-
   render() {
-    const { error, isLoaded, showForm, userDataArray, newUser, validation } = this.state;
-    const handleClick = this.handleClick;
-    const handleSubmit = this.handleSubmit;
-    const handleChange = this.handleChange;
+    const { error, isLoaded, showForm, userDataArray} = this.state;
 
     if (error) {
       return <div>Ошибка: {error.message}</div>
@@ -143,15 +86,10 @@ export class App extends Component {
             <Button 
               type="button"
               title="Добавить"
-              onClick={handleClick}
+              onClick={this.handleClick}
             />
             {showForm && (
-              <Form 
-                newUser={newUser} 
-                handleSubmit={handleSubmit}
-                onChange={handleChange}
-                formValid={validation.form}
-              />
+              <Form onSubmit={this.handleSubmit}/>
             )}
             <Table userDataArray={userDataArray} />
           </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,64 +5,195 @@ import { Input } from 'components/Input';
 import { Button } from 'components/Button';
 
 export class Form extends Component {
+
+  state = {
+    newData: {
+      id: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+    },
+      
+    validation: {
+      idValid: false,
+      firstNameValid: false,
+      lastNameValid: false,
+      emailValid: false,
+      phoneValid: false,
+      formValid: false,
+    }
+    
+  }
+
   static propTypes =  {
-    handleSubmit: PropTypes.func.isRequired,
-    onChange: PropTypes.func.isRequired,
-    formValid: PropTypes.bool.isRequired,
+    onSubmit: PropTypes.func,
+  }
+
+  handleChange = (event) => {
+    const fieldName = event.target.name;
+    const value = event.target.value;
+
+    this.setState(prevState => {
+      let newData = Object.assign({}, prevState.newData);
+      newData[fieldName] = value;
+      return { newData };
+    },
+    () => {this.validateField(fieldName, value)});
+  }
+
+  validateField = (fieldName, value) => {
+
+    // Валидация только на заполненность
+    switch(fieldName) {
+      case 'id':
+        this.setState({
+          validation: {
+            ...this.state.validation,
+            idValid: Boolean(value),
+          }
+        });
+        break;
+      case 'firstName':
+        this.setState({
+          validation: {
+            ...this.state.validation,
+            firstNameValid: Boolean(value),
+          }
+        });
+        break;
+      case 'lastName':
+        this.setState({
+          validation: {
+            ...this.state.validation,
+            lastNameValid: Boolean(value),
+          }
+        });
+        break;
+      case 'email':
+        this.setState(prevState => ({
+          validation: {
+            ...prevState.validation,
+            emailValid: Boolean(value),
+          }
+        }));
+        break;
+      case 'phone':
+        this.setState(prevState => ({
+          validation: {
+            ...prevState.validation,
+            phoneValid: Boolean(value),
+          }
+        }));
+        break;
+    }
+
+    this.validateForm();
+  }
+
+  validateForm = () => {
+    const { validation } = this.state;
+    let isValid = (validation.idValid && 
+        validation.firstNameValid && 
+        validation.lastNameValid && 
+        validation.emailValid && 
+        validation.phoneValid);
+    
+    this.setState(prevState => ({
+      validation: {
+        ...prevState.validation,
+        formValid: isValid,
+      }
+    }));
+  }
+
+  handleSubmit = (event) => {
+    const { onSubmit } = this.props;
+    const { newData } = this.state;
+
+    event.preventDefault();
+
+    if (typeof onSubmit === 'function') {
+      onSubmit(Object.assign({}, newData));
+      
+      this.setState({
+
+        id: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+
+        validation: {
+          idValid: false,
+          firstNameValid: false,
+          lastNameValid: false,
+          emailValid: false,
+          phoneValid: false,
+          formValid: false,
+        },
+      });
+    }
   }
 
   render() {
-    const { handleSubmit, onChange, formValid } = this.props;
+    const { formValid } = this.state.validation;
+    const { newData } = this.state;
     
     return (
       <Fragment>
-        <form className="form" method="post" onSubmit = { handleSubmit }>
+        <form className="form" method="post">
           <Input 
             type="text"
             title="id:"
             id="input-id"
             name="id"
+            value={this.state.id}
             placeholder="101"
-            onChange={onChange}
+            onChange={this.handleChange}
           />
           <Input 
             type="text"
             title="first name:"
             id="input-firstName"
             name="firstName"
+            value={this.state.firstName}
             placeholder="Sue"
-            onChange={onChange}
+            onChange={this.handleChange}
           />
           <Input 
              type="text"
              title="last name:"
              id="input-lastName"
              name="lastName"
+             value={this.state.lastName}
              placeholder="Corson"
-             onChange={onChange}
+             onChange={this.handleChange}
           />
           <Input 
              type="text"
              title="email:"
              id="input-email"
              name="email"
+             value={this.state.email}
              placeholder="dwhalley@in.gov"
-             onChange={onChange}
+             onChange={this.handleChange}
           />
           <Input 
              type="text"
              title="phone:"
              id="input-phone"
              name="phone"
+             value={this.state.phone}
              placeholder="(612)211-6296"
-             onChange={onChange}
+             onChange={this.handleChange}
           />
           <Button 
             action="#"
             type="submit"
             title="Добавить в таблицу"
             disabled={!formValid}
-            onClick={handleSubmit}
+            onClick={this.handleSubmit}
             />
         </form>
       </Fragment>
